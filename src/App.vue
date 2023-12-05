7c69a4d3ad3506e25aa07d8f55e9cd0436138d8f
smartsnake90 <smartsnake90@outlook.com>
2023-12-05 20:21:29 +0300

    update Section1 component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import Navigation from './components/Navigation.vue'
 import Section1 from './components/Section1.vue';
+import Section2 from './components/Section2.vue'
 </script>
 
 <template>
   <Navigation />
   <Section1 />
+  <Section2 />
 </template>
