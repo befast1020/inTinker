eaacda540b90e43f7593a0ba8a4efba08d0c0b5a
befast1020 <glorycream@outlook.com>
2023-12-06 14:36:33 +0300

    changed tab space for some components

--- src/components/CTab.vue
+++ src/components/CTab.vue
@@ -1,37 +1,37 @@
 <template>
-    <nav class="flex bg-[#F8F8F8] rounded-lg" aria-label="Tabs" role="tablist">
-        <button type="button"
-            :class="[current === 'tab1' && 'bg-white', 'w-1/4 justify-center m-[7px] py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-black text-opacity-[0.85] rounded-lg hover:bg-white']"
-            id="pills-on-gray-color-item-1" data-hs-tab="#pills-on-gray-color-1" aria-controls="pills-on-gray-color-1"
-            role="tab" @click="handleChange('tab1')">
-            Print Profile Manager
-        </button>
-        <button type="button"
-            :class="[current === 'tab2' && 'bg-white', 'w-1/4 justify-center m-[7px] py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-black text-opacity-[0.85] rounded-lg hover:bg-white']"
-            id="pills-on-gray-color-item-2" data-hs-tab="#pills-on-gray-color-2" aria-controls="pills-on-gray-color-2"
-            role="tab" @click="handleChange('tab2')">
-            Temperature Tower Generator
-        </button>
-        <button type="button"
-            :class="[current === 'tab3' && 'bg-white', 'w-1/4 justify-center m-[7px] py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-black text-opacity-[0.85] rounded-lg hover:bg-white']"
-            id="pills-on-gray-color-item-3" data-hs-tab="#pills-on-gray-color-3" aria-controls="pills-on-gray-color-3"
-            role="tab" @click="handleChange('tab3')">
-            Freestyle or AI Prints
-        </button>
-        <button type="button"
-            :class="[current === 'tab4' && 'bg-white', 'w-1/4 justify-center m-[7px] py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-black text-opacity-[0.85] rounded-lg hover:bg-white']"
-            id="pills-on-gray-color-item-4" data-hs-tab="#pills-on-gray-color-4" aria-controls="pills-on-gray-color-4"
-            role="tab" @click="handleChange('tab4')">
-            AI behavior
-        </button>
-    </nav>
+  <nav class="flex bg-[#F8F8F8] rounded-lg" aria-label="Tabs" role="tablist">
+    <button type="button"
+      :class="[current === 'tab1' && 'bg-white', 'w-1/4 justify-center m-[7px] py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-black text-opacity-[0.85] rounded-lg hover:bg-white']"
+      id="pills-on-gray-color-item-1" data-hs-tab="#pills-on-gray-color-1" aria-controls="pills-on-gray-color-1"
+      role="tab" @click="handleChange('tab1')">
+      Print Profile Manager
+    </button>
+    <button type="button"
+      :class="[current === 'tab2' && 'bg-white', 'w-1/4 justify-center m-[7px] py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-black text-opacity-[0.85] rounded-lg hover:bg-white']"
+      id="pills-on-gray-color-item-2" data-hs-tab="#pills-on-gray-color-2" aria-controls="pills-on-gray-color-2"
+      role="tab" @click="handleChange('tab2')">
+      Temperature Tower Generator
+    </button>
+    <button type="button"
+      :class="[current === 'tab3' && 'bg-white', 'w-1/4 justify-center m-[7px] py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-black text-opacity-[0.85] rounded-lg hover:bg-white']"
+      id="pills-on-gray-color-item-3" data-hs-tab="#pills-on-gray-color-3" aria-controls="pills-on-gray-color-3"
+      role="tab" @click="handleChange('tab3')">
+      Freestyle or AI Prints
+    </button>
+    <button type="button"
+      :class="[current === 'tab4' && 'bg-white', 'w-1/4 justify-center m-[7px] py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-black text-opacity-[0.85] rounded-lg hover:bg-white']"
+      id="pills-on-gray-color-item-4" data-hs-tab="#pills-on-gray-color-4" aria-controls="pills-on-gray-color-4"
+      role="tab" @click="handleChange('tab4')">
+      AI behavior
+    </button>
+  </nav>
 
-    <div class="mt-10">
-        <Section61 v-if="current === 'tab1'" />
-        <Section62 v-if="current === 'tab2'" />
-        <Section63 v-if="current === 'tab3'" />
-        <Section64 v-if="current === 'tab4'" />
-    </div>
+  <div class="mt-10">
+    <Section61 v-if="current === 'tab1'" />
+    <Section62 v-if="current === 'tab2'" />
+    <Section63 v-if="current === 'tab3'" />
+    <Section64 v-if="current === 'tab4'" />
+  </div>
 </template>
 <script lang="ts" setup>
 import Section61 from './Section6-1.vue';
@@ -42,16 +42,16 @@ import Section64 from './Section6-4.vue';
 
 <script lang="ts">
 export default {
-    data() {
-        return {
-            current: 'tab1'
-        }
-    },
-    methods: {
-        handleChange(tab: string) {
-            this.current = tab;
-        }
+  data() {
+    return {
+      current: 'tab1'
     }
+  },
+  methods: {
+    handleChange(tab: string) {
+      this.current = tab;
+    }
+  }
 }
 
 </script>
--- src/components/Section6-1.vue
+++ src/components/Section6-1.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="max-w-[1350px] m-auto">
-        <div class="flex justify-between">
-            <div class="flex-1">
-                <div class="font-Roboto font-bold text-sm">
-                    What is a Print Profile?
-                </div>
-                <div class="text-[14px] text-black text-opacity-60 max-w-screen-lg mt-4 font-bold">
-                    Think of a Print Profile as the starting and ending points of a Calibration. You start with a base Print
-                    Profile
-                    then do calibrations to make modifications on that Print Profile. Once the calibrations are done, the
-                    settings
-                    get used to update the Print Profile.
-                </div>
-                <img src="/img(sec6-1-1).svg" class="mt-4" />
-                <img src="/img(sec6-1-2).svg" class="mt-4" />
-                <button
-                    class="bg-regal-orange text-white text-opacity-[0.85] hover:text-opacity-100 text-xl font-bold py-2 px-4 rounded mt-5">
-                    Start Calibration for FREE
-                </button>
-            </div>
-            <div class="flex-1"><img src="/img(sec6-1-3).svg" /></div>
-
+  <div class="max-w-[1350px] m-auto">
+    <div class="flex justify-between">
+      <div class="flex-1">
+        <div class="font-Roboto font-bold text-sm">
+          What is a Print Profile?
         </div>
+        <div class="text-[14px] text-black text-opacity-60 max-w-screen-lg mt-4 font-bold">
+          Think of a Print Profile as the starting and ending points of a Calibration. You start with a base Print
+          Profile
+          then do calibrations to make modifications on that Print Profile. Once the calibrations are done, the
+          settings
+          get used to update the Print Profile.
+        </div>
+        <img src="/img(sec6-1-1).svg" class="mt-4" />
+        <img src="/img(sec6-1-2).svg" class="mt-4" />
+        <button
+          class="bg-regal-orange text-white text-opacity-[0.85] hover:text-opacity-100 text-xl font-bold py-2 px-4 rounded mt-5">
+          Start Calibration for FREE
+        </button>
+      </div>
+      <div class="flex-1"><img src="/img(sec6-1-3).svg" /></div>
+
     </div>
+  </div>
 </template>
