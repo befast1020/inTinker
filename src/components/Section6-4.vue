3e1a409c4599e95326ce2f0a435f91413adca63d
smartsnake90 <smartsnake90@outlook.com>
2023-12-08 04:57:40 +0300

    correct section6-4

--- src/components/Section6-4.vue
+++ src/components/Section6-4.vue
@@ -10,7 +10,36 @@
           Our AI recomends print settings to improve the quality of your prints. Customize the AI or use it right
           out of the box, the choice is yours.
         </div>
-        <img src="/img(sec6-4-1).svg" class="mt-4" />
+        <div>
+          <div class="flex mb-4">
+            <img src="/check.svg" class="self-start" />
+            <div>
+              <p class=" ml-4 font-bold">Setting Limits</p>
+              <p class="ml-4">Change the upper and lower limit of each setting. The AI won’t go outside these limits. This
+                allows you to section off areas that you know haven't worked in the past.</p>
+            </div>
+          </div>
+          <div class="flex mb-4">
+            <img src="/check.svg" class="self-start" />
+            <div>
+              <p class="ml-4 font-bold">Exploration Value</p>
+              <p class="ml-4">Change whether the AI picks values close to what it knows (good or bad) or picks newer far
+                out
+                values it hasn't seen before. If the AI keeps picking values that are close together, up the Exploration
+                Value and it will </p>
+            </div>
+
+          </div>
+          <div class="flex mb-4">
+            <img src="/check.svg" class="self-start" />
+            <div>
+              <p class="ml-4 font-bold">Resample</p>
+              <p class="ml-4">Not a fan of the suggested settings? Resample the AI for another set of settings using the
+                same criteria.</p>
+            </div>
+
+          </div>
+        </div>
         <button
           class="bg-regal-orange text-white text-opacity-[0.85] hover:text-opacity-100 text-xl font-bold py-2 px-4 rounded mt-5 hidden sm:block">
           Start Calibration for FREE
